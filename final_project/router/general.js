c201de2f5c88de103173defd43296ad32f95fcde
jmoukaila <jmoukaila@yahoo.fr>
2023-05-19 15:00:08 -0400

    new update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,14 +35,18 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const booksList = Object.entries(books);
+    for (const [key,book] of booksList) {
+        if (book.title == title)
+            return res.send(JSON.stringify(book, null, 4));
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;
